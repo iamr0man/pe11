8e123ffa4c6219b045d59759e6e5c1892ccfc15b
Korniienko Roman <koriny4@gmail.com>
2019-06-04 14:34:18 +0300

    homework4 | completely

--- javascript/homework/homework4_object/js/script.js
+++ javascript/homework/homework4_object/js/script.js
@@ -1,4 +1,4 @@
-const createNewUser = () => {
+function createNewUser() {
 
     let name = prompt("Insert your name: "),
         surname = prompt("Insert your surname: ");
@@ -12,41 +12,6 @@ const createNewUser = () => {
     const newUser = {
         firstName: name,
         lastName: surname,
-<<<<<<< HEAD
-        getLogin: function() {
-            return this.firstName[0].toLowerCase() + this.lastName.toLowerCase(); 
-        },
-    };
-
-        Object.defineProperties(newUser, {
-            firstName: {
-                writable: false,
-                configurable: true 
-            },
-            lastName: {
-                writable: false,
-                configurable: true
-            }
-        });
-
-        Object.defineProperties(newUser, {
-            setFirstName: {
-                value = 'Roman'
-            },
-            setLastName: {
-                value: 'Abramovich'
-            }
-        });
-
-        return newUser;           
-    }
-let newUser = createNewUser();
-console.log(newUser.getLogin());
-
-newUser.setFirstName("Roman");
-newUser.setLastName("Abramovich");
-
-=======
         getLogin: function () {
             return this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
         }
@@ -55,35 +20,17 @@ newUser.setLastName("Abramovich");
     Object.defineProperties(newUser, {
         firstName: {
             writable: false,
-            configurable: true
+            configurable: true,
+            enumerable: true
         },
         lastName: {
             writable: false,
-            configurable: true
+            configurable: true,
+            enumerable: true
         }
     });
-
-    Object.defineProperty(newUser, 'setFirstName', {
-        set: function(firstName){
-            this.firstName = firstName;
-        }
-    });
-
-    Object.defineProperty(newUser, 'setLastName', {
-        set: function(lastName){
-            newUser.lastName = lastName;
-        }
-    });
-
-
 
     return newUser;
 }
 let newUser = createNewUser();
 console.log(newUser.getLogin());
-debugger;
-newUser.setFirstName = "Roman";
-newUser.setLastName = "Roman";
->>>>>>> 665592192353209c38b8919d526c54c3a64d3074
-
-console.log(newUser);
