4f4cfbee6c459814f70abd17b1dacf1042aea157
Korniienko Roman <koriny4@gmail.com>
2019-06-07 20:26:39 +0300

    homework19_optional | completely

--- javascript/homework/homework4_object/js/script.js
+++ javascript/homework/homework4_object/js/script.js
@@ -1,7 +1,49 @@
+// function createNewUser() {
+
+//     let name = prompt("Insert your name: "),
+//         surname = prompt("Insert your surname: ");
+
+//     while (name.length === 0 || surname.length === 0) {
+//         alert("Your data incorrect. Try again!");
+//         name = prompt("Insert your name: ");
+//         surname = prompt("Insert your surname: ");
+//     }
+
+//     Object.defineProperty
+
+    // const newUser = {
+    //     firstName: name,
+    //     lastName: surname,
+//     getLogin: function () {
+//         return this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
+//     },
+//     setFirstName(name) {
+//         this.firstName = name;
+//     }
+// };
+
+//     Object.defineProperties(newUser, {
+//         firstName: {
+//             writable: false
+//         },
+//         lastName: {
+//             writable: false,
+//             configurable: true,
+//             enumerable: true
+//         }
+//     });
+
+//     return newUser;
+// }
+// let newUser = createNewUser();
+// // console.log(newUser.getLogin());
+// console.dir(newUser);
+
+
 function createNewUser() {
 
-    let name = prompt("Insert your name: "),
-        surname = prompt("Insert your surname: ");
+    let name = prompt("Insert your name"),
+        surname = prompt("Insert your surname");
 
     while (name.length === 0 || surname.length === 0) {
         alert("Your data incorrect. Try again!");
@@ -9,28 +51,25 @@ function createNewUser() {
         surname = prompt("Insert your surname: ");
     }
 
-    const newUser = {
-        firstName: name,
-        lastName: surname,
-        getLogin: function () {
-            return this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
+    const newUser = new Object();
+    this.firstName = name;
+    this.lastName = surname;
+    this.getLogin = function () {
+        return (this.firstName[0] + this.lastName).toLowerCase();
+    }
+    this.setFirstName = function(newFirstName) {
+        debugger;
+            Object.defineProperty(this, newUser.firstName, {
+                writable: true,
+                value: newFirstName
+            })
         }
-    };
 
-    Object.defineProperties(newUser, {
-        firstName: {
-            writable: false,
-            configurable: true,
-            enumerable: true
-        },
-        lastName: {
-            writable: false,
-            configurable: true,
-            enumerable: true
-        }
-    });
-
-    return newUser;
+    Object.defineProperty(newUser, newUser.firstName, {
+        writable: false
+    })
 }
-let newUser = createNewUser();
-console.log(newUser.getLogin());
+
+let newUser = new createNewUser();
+newUser.setFirstName("Roman");
+console.log(newUser);
