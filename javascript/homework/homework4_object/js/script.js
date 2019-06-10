e2b4ea159c0c8a6e3cf4acb9a5958b823061b231
Korniienko Roman <koriny4@gmail.com>
2019-06-10 13:36:08 +0300

    homework4 | add set mathod

--- javascript/homework/homework4_object/js/script.js
+++ javascript/homework/homework4_object/js/script.js
@@ -1,45 +1,3 @@
-// function createNewUser() {
-
-//     let name = prompt("Insert your name: "),
-//         surname = prompt("Insert your surname: ");
-
-//     while (name.length === 0 || surname.length === 0) {
-//         alert("Your data incorrect. Try again!");
-//         name = prompt("Insert your name: ");
-//         surname = prompt("Insert your surname: ");
-//     }
-
-//     Object.defineProperty
-
-    // const newUser = {
-    //     firstName: name,
-    //     lastName: surname,
-//     getLogin: function () {
-//         return this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
-//     },
-//     setFirstName(name) {
-//         this.firstName = name;
-//     }
-// };
-
-//     Object.defineProperties(newUser, {
-//         firstName: {
-//             writable: false
-//         },
-//         lastName: {
-//             writable: false,
-//             configurable: true,
-//             enumerable: true
-//         }
-//     });
-
-//     return newUser;
-// }
-// let newUser = createNewUser();
-// // console.log(newUser.getLogin());
-// console.dir(newUser);
-
-
 function createNewUser() {
 
     let name = prompt("Insert your name"),
@@ -50,26 +8,44 @@ function createNewUser() {
         name = prompt("Insert your name: ");
         surname = prompt("Insert your surname: ");
     }
-
-    const newUser = new Object();
-    this.firstName = name;
-    this.lastName = surname;
-    this.getLogin = function () {
+    const user = {};
+    Object.defineProperties(user, {
+        'firstName': {
+            value: name,
+            writable: false,
+            configurable: true
+        },
+        'lastName': {
+            value: surname,
+            writable: false,
+            configurable: true
+        }
+    });
+    user.getLogin = function () {
         return (this.firstName[0] + this.lastName).toLowerCase();
     }
-    this.setFirstName = function(newFirstName) {
-        debugger;
-            Object.defineProperty(this, newUser.firstName, {
-                writable: true,
-                value: newFirstName
-            })
-        }
-
-    Object.defineProperty(newUser, newUser.firstName, {
-        writable: false
-    })
+    user.setFirstName = function (value) {
+        Object.defineProperty(user, 'firstName', {
+            writable: true,
+            value: value
+        });
+        Object.defineProperty(user, 'firstName', {
+            writable: false
+        });
+    };
+    user.setLastName = function (value) {
+        Object.defineProperty(user, 'lastName', {
+            writable: true,
+            value: value
+        });
+        Object.defineProperty(user, 'lastName', {
+            writable: false
+        });
+    };
+    return user;
 }
 
 let newUser = new createNewUser();
+console.log(newUser.getLogin());
 newUser.setFirstName("Roman");
 console.log(newUser);
--- javascript/snippeth/snippeth_array/script.js
+++ javascript/snippeth/snippeth_array/script.js
@@ -6,32 +6,81 @@
 
 // (function hello() { return 1; }); alert(hello); 
 
-let arr = new Array("Wind", "Rain", "Fire");
-const str = arr.join(' / ');
+// let arr = new Array("Wind", "Rain", "Fire");
+// const str = arr.join(' / ');
 
-arr.push("Earth");
+// arr.push("Earth");
 
 // myArray.push("Semptember"); 
 // myArray.pop();
 
 
 // const firstElemment = arr.shift();
-arr.unshift('Human', 'Weapon');
+// arr.unshift('Human', 'Weapon');
 
-arr = arr.slice(2, arr.length); //first version
+// arr = arr.slice(2, arr.length); //first version
 
 // arr.slice(1, 4, 'a', 'b', 'c');
 
 // arr.forEach(e => console.log(e));
 
-const numbers = [79, 2, 12, 9];
-const arrMap = numbers.map(e => e % 2 === 0 ? e+1 : e+2);
-arrMap.forEach(e => console.log(e));
+// const numbers = [79, 2, 12, 9];
+// const arrMap = numbers.map(e => e % 2 === 0 ? e+1 : e+2);
+// arrMap.forEach(e => console.log(e));
 
-var a1 = ['a', 10, 'b', 20, 'c', 30];
-var a2 = a1.filter(function(item) { return typeof item == 'number'; });
-console.log(a2);
+// var a1 = ['a', 10, 'b', 20, 'c', 30];
+// var a2 = a1.filter(function(item) { return typeof item == 'number'; });
+// console.log(a2);
 
-var a = [10, 20, 30];
-var total = a.reduce(function(first, second, index) { return first + second; }, 0);
-console.log(total)
+// var a = [10, 20, 30];
+// var total = a.reduce(function(first, second, index) { return first + second; }, 0);
+// console.log(total)
+
+// function descendingOrder(array) {
+//     debugger;
+//     array = ('' + array).split('');
+//     let length = array.length,
+//         template = 0;
+//     for (let i = 0; i < length; i++) {
+//         for (let j = 0; j < length - i - 1; j++) {
+//             if (array[j] < array[j + 1]) {
+//                 template = array[j];
+//                 array[j] = array[j+1];
+//                 array[j+1] = template;
+//             }
+//         }
+//     }
+//     return +array.join('');
+// }
+
+// console.log(descendingOrder(987))
+
+// function descendingOrder(number) {
+//     array = ('' + number).split('').array.sort((a, b) => {
+//         if (a > b) {
+//             return -1;
+//         }
+//         if (a < b) {
+//             return 1;
+//         }
+//     });
+//     return +array.join('');
+// }
+
+function DNAStrand(dna){
+    let result = dna.split('').map((element,index, array) => {
+        array[index] = element.allReplace({'A' :'T', 'C':'G', 'T': 'A', 'G':'C'});
+    })
+    return result;
+}
+
+String.prototype.allReplace = function(obj) {
+    let currentChar = this;
+    for (let x in obj) {
+        currentChar = currentChar.replace(new RegExp(x, 'g'), obj[x]);
+    }
+    return currentChar;
+};
+
+debugger;
+console.log(DNAStrand ("ATTGC"));
