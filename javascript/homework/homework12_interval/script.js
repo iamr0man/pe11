38956d2e93b778ac471489417b2fc4e660159da6
Korniienko Roman <koriny4@gmail.com>
2019-06-28 16:35:00 +0300

    homework12 | completely

---
+++ javascript/homework/homework12_interval/script.js
@@ -0,0 +1,42 @@
+const $firstElem = $('.image-to-show').eq(0)
+let i = 0;
+
+$firstElem.fadeIn().show();
+
+
+let interval = setInterval(carousel, 1200);
+
+function carousel() {
+    const $items = $('.image-to-show');
+    console.log('tick')
+    $items.eq(i).fadeOut().hide();
+    $items.eq(++i).fadeIn().show();
+    if (i === $items.length) {
+        i = 0;
+        $firstElem.fadeIn().show();
+    }
+}
+
+
+
+const pause = document.createElement('button')
+pause.innerText = 'Pause'
+document.body.appendChild(pause);
+
+const play = document.createElement('button')
+play.innerText = 'Continue'
+document.body.append(play);
+
+let isPaused = false;
+
+$(pause).click(function () {
+    clearInterval(interval)
+    isPaused = true;
+});
+
+$(play).click(function () {
+    if (isPaused) {
+        interval = setInterval(carousel, 1200);
+        isPaused = false;
+    }
+});
