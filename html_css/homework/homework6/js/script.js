cae39af9df3395406091d09f7fe9f6cdc74be115
Korniienko Roman <koriny4@gmail.com>
2019-06-29 16:46:44 +0300

    homework13 | fix bullshit code

--- html_css/homework/homework6/js/script.js
+++ html_css/homework/homework6/js/script.js
@@ -1,30 +1,23 @@
+let children = document.head.children[3];
+
 const btn = document.createElement('button');
 btn.innerText = 'Change Theme';
 btn.className = 'change-color-btn'
 document.body.appendChild(btn);
 
-const headline = document.querySelectorAll('.headline');
-const container = document.querySelectorAll('.container');
-
 window.onload = function checkLocal(){
-
-    if(localStorage.getItem('color')){
-        headline.forEach(e => e.style.color  = localStorage.getItem('color'));
-        container.forEach(e => e.style.backgroundColor  = localStorage.getItem('backgroundColor'));
+    if(localStorage.length>0){
+        children.href = localStorage.getItem("theme");
     }    
 }
-
 btn.addEventListener('click', changeTheme);
 
-let isNewTheme = false;
-
 function changeTheme(){
-
-    if(!isNewTheme) {
-        document.head.children[3].href = 'css/newTheme.css';
-        isNewTheme = true;
+        if(localStorage.length===0){
+        localStorage.setItem("theme", 'css/newTheme.css')
+        window.location.reload();
     } else {
-        document.head.children[3].href = 'css/style.css';
-        isNewTheme = false;
+        localStorage.clear();
+        window.location.reload();
     }
 }
