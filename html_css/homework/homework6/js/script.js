007e27fbd5648691dfc2a39ba4232a61e281875d
iamr0man <koriny4@gmail.com>
2019-06-30 21:27:12 +0300

    homework15 | add slow scroll and hide section

--- html_css/homework/homework6/js/script.js
+++ html_css/homework/homework6/js/script.js
@@ -1,23 +1,43 @@
-let children = document.head.children[3];
+// const btn = document.createElement('button');
+// btn.innerText = 'Change Theme';
+// btn.className = 'change-color-btn'
+// document.body.appendChild(btn);
 
-const btn = document.createElement('button');
-btn.innerText = 'Change Theme';
-btn.className = 'change-color-btn'
-document.body.appendChild(btn);
+// window.onload = function checkLocal(){
+//     if(localStorage.length>0){
+//         children.href = localStorage.getItem("theme");
+//     }    
+// }
+// btn.addEventListener('click', changeTheme);
 
-window.onload = function checkLocal(){
-    if(localStorage.length>0){
-        children.href = localStorage.getItem("theme");
-    }    
-}
-btn.addEventListener('click', changeTheme);
+// function changeTheme(){
+//     if(localStorage.length===0){
+//         localStorage.setItem("theme", 'css/newTheme.css')
+//         window.location.reload();
+//     } else {
+//         localStorage.clear();
+//         window.location.reload();
+//     }
+// }
 
-function changeTheme(){
-        if(localStorage.length===0){
-        localStorage.setItem("theme", 'css/newTheme.css')
-        window.location.reload();
+const $btn = $('.to-top-btn');
+
+$(document).scroll(function(e){
+
+    let $clientHeight = $(window).innerHeight();
+    if($(window).scrollTop() > $clientHeight){
+        $btn.fadeIn();
     } else {
-        localStorage.clear();
-        window.location.reload();
+        $btn.fadeOut();
     }
-}
+});
+
+$btn.click(function(){
+    $('html').animate({
+        scrollTop: 0
+    }, 500)
+})
+
+$('.hide-content-btn').click(function(){
+    $('#block-hot-news').slideToggle(600);
+})
