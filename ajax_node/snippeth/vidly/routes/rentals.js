6dbc3bfeb21e68e7d7c8e2fd8754d5c297d0c0fe
iamr0man <koriny4@gmail.com>
2019-09-17 12:40:12 +0300

    add model rentals

--- ajax_node/snippeth/vidly/routes/rentals.js
+++ ajax_node/snippeth/vidly/routes/rentals.js
@@ -1,9 +1,19 @@
-const {Rental, validate} = require('../models/rental'); 
-const {Movie} = require('../models/movie'); 
-const {Customer} = require('../models/customer'); 
+const {
+  Rental,
+  validate
+} = require('../models/rental');
+const {
+  Movie
+} = require('../models/movie');
+const {
+  Customer
+} = require('../models/customer');
 const mongoose = require('mongoose');
+const Fawn = require('fawn')
 const express = require('express');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
@@ -11,7 +21,9 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+  const {
+    error
+  } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const customer = await Customer.findById(req.body.customerId);
@@ -22,10 +34,10 @@ router.post('/', async (req, res) => {
 
   if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
 
-  let rental = new Rental({ 
+  let rental = new Rental({
     customer: {
       _id: customer._id,
-      name: customer.name, 
+      name: customer.name,
       phone: customer.phone
     },
     movie: {
@@ -34,12 +46,21 @@ router.post('/', async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate
     }
   });
-  rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
+  try{
+    new Fawn.Task()
+    .save('rentals', rental)
+    .update('movies', { _id: movie._id}, {
+      $inc: {
+        numberInStock: -1
+      }
+    })
+    .run();
   
   res.send(rental);
+  } catch(ex){
+    res.status(500).send('Something failed')
+  }
 });
 
 router.get('/:id', async (req, res) => {
@@ -50,4 +71,4 @@ router.get('/:id', async (req, res) => {
   res.send(rental);
 });
 
-module.exports = router; 
+module.exports = router;
