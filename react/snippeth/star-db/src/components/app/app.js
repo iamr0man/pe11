c7df4ed3b4736959511b1bb10fabf4a7bf1adf07
iamr0man <koriny4@gmail.com>
2019-10-04 13:14:18 +0300

    try to use childen property

--- react/snippeth/star-db/src/components/app/app.js
+++ react/snippeth/star-db/src/components/app/app.js
@@ -5,12 +5,15 @@ import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
 import PeoplePage from '../people-page';
-import ItemList from '../item-list'
-import PersonDetails from '../person-details'
+// import ItemList from '../item-list'
+// import PersonDetails from '../person-details'
 
 import './app.css';
+import SwapiService from '../../services/swapi-service';
 
 export default class App extends Component {
+
+  swapiService = new SwapiService();
 
   state = {
     showRandomPlanet: true,
@@ -55,14 +58,27 @@ export default class App extends Component {
 
         <PeoplePage />
 
-        <div className="row mb2">
+        {/* <div className="row mb2">
           <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
+            <ItemList onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => (<span>{item.name}<button>!</button></span>)} />
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
           </div>
         </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div> */}
 
       </div>
     );
