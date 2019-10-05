732f6ba24548bbf1ba34d0d40a7f1e8e8ec6d04c
iamr0man <koriny4@gmail.com>
2019-10-05 16:13:56 +0300

    work with hoc

--- react/snippeth/star-db/src/components/people-page/people-page.js
+++ react/snippeth/star-db/src/components/people-page/people-page.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 
 import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
-import ErrorIndicator from '../error-indicator/error-indicator';
-import Row from '../row'
-import ErrorBoundry from '../error-boundry'
+import ItemDetails from '../item-details/item-details';
+import SwapiService from '../../services/swapi-service';
+import Row from '../row';
+import ErrorBoundry from '../error-boundry';
 
 import './people-page.css';
-import SwapiService from "../../services/swapi-service";
 
 export default class PeoplePage extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    selectedPerson: 3,
+    selectedPerson: 11
   };
 
   onPersonSelected = (selectedPerson) => {
@@ -23,25 +22,26 @@ export default class PeoplePage extends Component {
 
   render() {
 
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
-    }
-
     const itemList = (
-      <ItemList onItemSelected={this.onPersonSelected}
+      <ItemList
+        onItemSelected={this.onPersonSelected}
         getData={this.swapiService.getAllPeople}>
-        {(i) => `${i.name} (${i.birthYear})`}
+
+        {(i) => (
+          `${i.name} (${i.birthYear})`
+        )}
+
       </ItemList>
-    )
+    );
 
     const personDetails = (
-      <PersonDetails personId={this.state.selectedPerson} />
-    )
+      <ErrorBoundry>
+        <ItemDetails itemId={this.state.selectedPerson} />
+      </ErrorBoundry>
+    );
 
     return (
-      <ErrorBoundry>
-        <Row left={itemList} right={personDetails} />
-      </ErrorBoundry>
+      <Row left={itemList} right={personDetails} />
     );
   }
 }
