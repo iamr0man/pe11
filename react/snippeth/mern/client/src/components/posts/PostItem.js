06bbde67b874d6be2c6e53d8135f659b77cddd67
iamr0man <koriny4@gmail.com>
2019-10-19 16:32:51 +0300

    add action add post

--- react/snippeth/mern/client/src/components/posts/PostItem.js
+++ react/snippeth/mern/client/src/components/posts/PostItem.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike, } from '../../actions/post';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
     addLike,
     removeLike,
-    // deletePost,
+    deletePost,
     auth,
     post: { _id, text, name, avatar, user, likes, comments, date },
     // showActions
@@ -50,7 +50,7 @@ const PostItem = ({
                     </Link>
                     {!auth.loading && user === auth.user._id && (
                         <button
-                            // onClick={() => deletePost(_id)}
+                            onClick={() => deletePost(_id)}
                             type='button'
                             className='btn btn-danger'>
                             <i className='fas fa-times' />
@@ -68,7 +68,7 @@ PostItem.propTypes = {
     auth: PropTypes.object.isRequired,
     addLike: PropTypes.func.isRequired,
     removeLike: PropTypes.func.isRequired,
-    // deletePost: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
     // showActions: PropTypes.bool
 }
 
@@ -78,5 +78,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    { addLike, removeLike }
+    { addLike, removeLike, deletePost }
 )(PostItem);
