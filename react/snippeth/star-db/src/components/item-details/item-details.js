2e6aa98251bada448ef798c6bbae77c8dbd31776
iamr0man <koriny4@gmail.com>
2019-10-27 20:14:12 +0200

    fix some bug

--- react/snippeth/star-db/src/components/item-details/item-details.js
+++ react/snippeth/star-db/src/components/item-details/item-details.js
@@ -71,6 +71,7 @@ export default class ItemDetails extends Component {
           <ul className="list-group list-group-flush">
             {
               React.Children.map(this.props.children, (child) => {
+                console.log(child)
                 return React.cloneElement(child, { item });
               })
             }
