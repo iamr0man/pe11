cd7cce49cb8519754be7ee7147627dbe96411a1d
iamr0man <koriny4@gmail.com>
2019-11-05 12:12:32 +0200

    add pagination

--- ajax_node/snippeth/bootcamp/server.js
+++ ajax_node/snippeth/bootcamp/server.js
@@ -6,6 +6,7 @@ const connectDB = require('./config/db')
 const errorHandler = require('./middleware/error')
 
 const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
@@ -25,6 +26,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
