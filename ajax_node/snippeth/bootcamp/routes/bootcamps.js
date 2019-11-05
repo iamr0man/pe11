3579a2e928c6cbf7e3de225e44632bcf9afb9ada
iamr0man <koriny4@gmail.com>
2019-11-05 12:45:02 +0200

    interactive bootcamp with courses

--- ajax_node/snippeth/bootcamp/routes/bootcamps.js
+++ ajax_node/snippeth/bootcamp/routes/bootcamps.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps')
 
+// Inlude other resourse routers
+const courseRouter = require('./courses')
+
 const router = express.Router();
+
+// Re-route into other resourse routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
