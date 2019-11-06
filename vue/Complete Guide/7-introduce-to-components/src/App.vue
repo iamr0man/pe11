25666e1c0acc5e4fa82a529b7f7b1ad74b66148c
iamr0man <koriny4@gmail.com>
2019-11-06 22:00:24 +0200

    familiar with components

---
+++ vue/Complete Guide/7-introduce-to-components/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="index in 5" :key="index">Server #{{ index }}</li>
+        </ul>
+      </div>
+      <app-server-details></app-server-details>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Shared/Header.vue";
+import Footer from "./components/Shared/Footer.vue";
+import Servers from "./components/Server/Servers.vue";
+import ServerDetails from "./components/Server/ServerDetails.vue";
+
+export default {
+  components: {
+    appHeader: Header,
+    Servers,
+    "app-server-details": ServerDetails,
+    "app-footer": Footer
+  }
+};
+</script>
+
+<style>
+</style>
