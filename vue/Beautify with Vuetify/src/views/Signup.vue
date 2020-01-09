18022a9eeab3a4be2bcd1dfae2b13fd50285fad5
iamr0man <koriny4@gmail.com>
2020-01-09 19:48:16 +0200

    additional to mass upload

---
+++ vue/Beautify with Vuetify/src/views/Signup.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Signup</h1>
+        <v-form>
+          <v-text-field label="Email" type="email"></v-text-field>
+          <v-select
+            label="Which browser do you use?"
+            :items="browsers"
+            v-model="currentBrowser"
+          />
+          <template v-if="currentBrowser==='Cash' || currentBrowser==='Student'">
+            <v-file-input label="Attach profile picture"></v-file-input>
+            <v-text-field
+              v-model="birthday"
+              label="Birthday"
+              readonly
+            ></v-text-field>
+            <v-date-picker v-model="birthday"></v-date-picker>
+            <template v-if="currentBrowser==='Student'">
+              <v-checkbox label="Agree to terms &amp; conditions"></v-checkbox>
+            </template>
+          </template>
+          <v-btn type="submit" color="primary">Submit</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    birthday: '',
+    browsers: ['Cash', 'Split', 'Wargaming', 'Student'],
+    currentBrowser: ''
+  }),
+}
+</script>
